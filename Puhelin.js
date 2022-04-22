1fd5e4270abcda03d2144b58bb774ad638d132e5
Aldrinzog <97434097+Aldrinzog@users.noreply.github.com>
2022-04-22 13:23:32 +0300

    Add files via upload

---
+++ Puhelin.js
@@ -0,0 +1,37 @@
+var viesti = "Moikka Vantaa!";
+console.log(viesti);
+let i =5555;
+for (let i = 0; i < 10; i++)
+{
+}
+console.log("Silmukan ulkopuolella iin arvo on = " + i );
+
+let luku1 = "3";
+let luku2 = 3;
+let summa = luku1 + luku2;
+if (luku1==luku2) {
+    console.log("luvut ovat samat ja samaa tyyppiä");
+} else console.log("Luvut ovat eri suuret tai eri tyyppiä")
+console.log("-------------------------------------")
+
+var puhelimet = ["Apple", "Samsung", "Nokia",]
+for (Var j = 0, j < puhelimet.length; j++) {
+    if (puhelimet[j]) == "Nokia")  { 
+    console.log(Puhelimet[j] + "<-- Minulla on tämä puhelin  ");
+    {
+       else console.log(Puhelimet[j]);
+}
+console.log("-------------------------------------")
+console.log(typeof("Maisa"));
+console.log(typeof("89"));
+console.log(typeof(89));
+console.log("Luvun 1 tyyppi on = " + typeof(luku1));
+console.log("Luvun 1 tyyppi on = " + typeof(luku2));
+
+console.log("-------------------------------------")
+var merkkijono = "Tämö on javaScript -Tehtävä eikös vain?";
+var positio = merkkijono.indexOf("-");
+console.log(merkkijono);
+console.log("tavuviiva on positiossa: " + positio);
+
+console.log(merkkijono.slice(positio+1,merkkijono.length);
