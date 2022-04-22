4a72a962b641d81822ace3919fe4c943bdca19eb
Aldrinzog <97434097+Aldrinzog@users.noreply.github.com>
2022-04-22 13:23:44 +0300

    Add files via upload

---
+++ Numero.js
@@ -0,0 +1,14 @@
+console.log("Testi");
+
+var numerot = ""
+var i=0;
+do {
+    numerot = "Numero on: " + i;
+    console.log(numerot);
+    i++
+}
+while (i < 10);{
+    numerot = "Numero on: " + j;
+    console.log(numerot);
+    j++
+}
